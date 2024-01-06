c42132029564f2be74ebc45d685016983de3346b
Shane Martin <shane.martin@plooto.com>
2024-01-06 14:21:58 -0500

    fix: remove SQUEEZELITE var from script

--- squeezelite/start.sh
+++ squeezelite/start.sh
@@ -1,7 +1,5 @@
 #!/bin/env bash
-SQUEEZELITE="$HOME/src/squeezelite/squeezelite"
-
-OUTPUT_DEVICE="$($SQUEEZELITE -l | grep Anker | awk '{$1=$1};1' | cut -d " " -f 1)"
+OUTPUT_DEVICE="$($HOME/src/squeezelite/squeezelite -l | grep Anker | awk '{$1=$1};1' | cut -d " " -f 1)"
 PLAYER_NAME=$1
 
-$SQUEEZELITE -o $OUTPUT_DEVICE -n $PLAYER_NAME
+$HOME/src/squeezelite/squeezelite -o $OUTPUT_DEVICE -n $PLAYER_NAME
